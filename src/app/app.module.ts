0aa6363a6fdb5618fb3d4a56c3a1837c9c097141
Carolina <carolina.gonzalez15@outlook.com>
2023-06-01 23:01:55 -0500

    Hero creación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeroComponent } from './home/hero/hero.component';
+import { HeaderComponent } from './home/header/header.component';
+import { FooterComponent } from './home/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
